0701383615218e24ee25362e01ee417db281c36b
Michel Vielmetter <mvielmetter@rsteinmann.de>
2015-02-04 11:06:18 +0100

    Establish Env

---
+++ sample.env.php
@@ -0,0 +1,32 @@
+<?php
+/*!!*
+╔================================ sample.env.php =======================================================================
+║
+║ 	Create Date: 04.02.2015
+║
+║ 	Last Update: 04.02.2015
+║
+║ 	Author(s): Michel Vielmetter <coding@michelvielmetter.de>
+║ 	Copyright: Michel Vielmetter 2015
+║ 	Licence:   MIT
+║
+╠================================ sample.env.php =======================================================================
+║
+║ 	[packageID]                 accountbook.environment
+║
+╚================================ sample.env.php =======================================================================
+*/
+
+
+/*
+╔=======================================================================================================================
+║
+║ 	All Values should be replaced to make the the app work
+║
+╚=======================================================================================================================
+*/
+
+return array(
+    // The Password of your Database (change Database name & user in environment settings
+    'DB_PASSWORD' => '',
+);
